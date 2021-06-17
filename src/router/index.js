c5c604f0d211f04fbfa26e8b240f983531268998
josiLopera <joxi.lopera@gmail.com>
2021-06-16 20:44:17 -0500

    using axios to fetch one profile with cors problem

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Profiles from '../views/Profiles.vue'
+import Jobs from '../views/Jobs.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Profiles',
+    component: Profiles
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
   }
 ]
 
---
+++ src/views/Profiles.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <button @click="getProfiles">get profiles</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+      arrayProfiles: []
+    }
+  },
+  methods: {
+    getProfiles () {
+      axios
+        .get('https://bio.torre.co/api/bios/dianacarvajalrios', {
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          }
+        })
+        .then((result) => {
+          console.log(result.data)
+        })
+        .catch(e => console.log(e))
+    }
+  }
+}
+</script>
